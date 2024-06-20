1d486013d2b697a29e90b17710e64a6ccb6fbbc1
mac <5SAJIM26@solent.ac.uk>
2024-06-20 11:03:19 +0100

    String variables and objects compared

--- IdenticalNeighbors/src/main/java/com/sparta/ms/App.java
+++ IdenticalNeighbors/src/main/java/com/sparta/ms/App.java
@@ -4,7 +4,6 @@ public class App {
 
     public static boolean findNeighbors(int[] inArray) {
         if (inArray != null) {
-
             for (int i = 0; i < inArray.length - 2; i++) {
                 if (inArray[i] == inArray[i + 1] && inArray[i] == inArray[i + 2]) {
                     return true;
